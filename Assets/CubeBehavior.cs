0089650263f5f5ac5610b046986fc9ee8db785c2
MiffedSheep(MattG) <mwg13@hampshire.edu>
2013-11-25 18:22:52 -0500

    First bits of code

---
+++ Assets/CubeBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehavior : MonoBehaviour {
+public int x, y;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
